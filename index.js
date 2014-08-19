e69d3f58f44cafa3b0482edface049e1d8407b95
cfb65 <cfb.65@163.com>
2014-08-19 17:31:30 +0800

    initial urltitle

---
+++ index.js
@@ -0,0 +1,63 @@
+var request=require('request'),
+    Q=require('q'),
+    jsdom = require("jsdom"),
+    fs=require('fs');
+var jquery = fs.readFileSync("./node_modules/jquery/dist/jquery.js", "utf-8");
+function crawlUrl(url) {
+	var deferred = Q.defer();
+	var options={
+		url:url,
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'
+		}
+	}
+	request(options,function(error, response, body){
+           if(error) {
+              deferred.reject(error);
+           }
+           else deferred.resolve(body);
+    });
+    return deferred.promise;
+}
+function getTitleFromBody(body){
+	var deferred = Q.defer();
+    jsdom.env({
+    	html:body,
+    	src:[jquery],
+    	done:function (error, window) {
+      	 if(error) {
+              deferred.reject(error);
+         }
+         else {
+         	var title=window.$("title").text();
+         	if(title)
+         	  deferred.resolve(title);
+            else{
+            	throw new Error('not find title');
+            }
+         };
+        }
+    });
+    return deferred.promise;
+}
+function getTitleUrl(url){
+	return crawlUrl(url).then(getTitleFromBody);
+}
+module.exports = function getTitleFromUrl(urls, callback) {
+  var promise;
+
+  if('string' === typeof urls) {
+    promise = getTitleUrl(urls);
+  } else {
+    promise = Q.all(urls.map(function(url){return getTitleUrl(url)}));
+  }
+  if(!callback) {
+    return promise;
+  }
+
+  promise.then(function(result) {
+    callback(null, result);
+  }, function(err) {
+    callback(err);
+  }).done();
+}
---
+++ test/index.js
@@ -0,0 +1,45 @@
+var should = require('should');
+var getTitleFromUrl = require('../index');
+
+describe('getTitleFromUrl', function() {
+	it('should get baidu title', function(done) {
+		getTitleFromUrl('http://www.baidu.com/',function(err,title){
+		   if(err){
+		   	 done(err);
+		   }	
+		   title.should.equal("百度一下，你就知道 ");
+		   done();
+		});
+	});
+	it('should get baidu title with promise', function(done) {
+      getTitleFromUrl('http://www.baidu.com/')
+      .then(function(title) {
+        title.should.equal("百度一下，你就知道");       
+      }).then(function(){
+      	done();
+      },function(err){
+      	done(err);
+      });
+    });
+    it('should get baidu and 36kr title', function(done) {
+		getTitleFromUrl(['http://www.baidu.com/','http://www.36kr.com'],function(err,title){
+		   if(err){
+		   	 done(err);
+		   }			
+		    title[0].should.equal("百度一下，你就知道");
+			title[1].should.equal("36氪 | 关注互联网创业");   
+			done();  
+		})
+	});
+	it('should get baidu and 36kr title with promise', function(done) {
+      getTitleFromUrl(['http://www.baidu.com/','http://www.36kr.com'])
+      .then(function(title) {
+        title[0].should.equal("百度一下，你就知道");
+        title[1].should.equal("36氪 | 关注互联网创业");      
+      }).then(function(){
+      	done();
+      },function(err){
+      	done(err);
+      });
+    });
+});
